7b32de448c4f5e5e51dcaf1861983785294eb7b7
Monisha-Singhal <tarusinghal945@gmail.com>
2025-04-16 19:02:07 +0530

    game

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -9,6 +9,10 @@ public class myFirstGame {
 
         Scanner sc= new Scanner(System.in);
         String permisson= sc.nextLine();
+        System.out.println("==============================");
+        System.out.println("🎲 Rolling the dice...");
+
+
 
 
 
@@ -16,31 +20,41 @@ public class myFirstGame {
             Random rn = new Random();
             int number = rn.nextInt(0, 7);
             System.out.println("You rolled to number " +number);
+
+            System.out.println("==============================");
             if(number==6){
 
                 initialAmount=initialAmount+50;
                 System.out.println("Hurrah! you won 50 more coins🎉🎉🎉🎉");
                 System.out.println("your total coins are "+initialAmount+ "🪙🪙🪙🪙");
 
+                System.out.println("==============================");
+
             }
           if (number==5){
               initialAmount=initialAmount+40;
               System.out.println("Hurrah! you won 40 more coins🎉🎉🎉🎉");
               System.out.println("your total coins are "+initialAmount+ "🪙🪙🪙🪙");
+
+              System.out.println("==============================");
           }
           if(number>0 && number<=4){
               initialAmount=initialAmount+20;
               System.out.println("Hurrah! you won 20 more coins🎉🎉🎉🎉");
               System.out.println("your total coins are "+initialAmount+ "🪙🪙🪙🪙");
+
+              System.out.println("==============================");
           }
           if(number==0){
               initialAmount=initialAmount-5;
               System.out.println("oops! you lose your 5 coins.😔😔😔");
               System.out.println("your coins are "+initialAmount+ "🪙");
               System.out.println("Please Try Again Later");
+              System.out.println("==============================");
           }
         }
         System.out.println("THANK YOU!!!!!");
+        System.out.println("==============================");
 
 
     }
